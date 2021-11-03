97b2634ee3bb82b9a391b6ee24b5fd663fd4e765
John Lahut <john.lahut@wabtec.com>
2021-11-03 10:46:25 -0400

    ag-grid missing data example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+import badChartData from "./data/not-working-data.json"
+import goodChartData from "./data/working-data.json"
+
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { LicenseManager } from 'ag-grid-enterprise';
+import { GridOptions } from 'ag-grid-community';
 
 function App() {
+
+  const columns = badChartData.columns.filter(col => col.name === 'MP_1005_N_0_0' || col.name === 'OBJID')
+  const gridOptions: GridOptions = {
+    enableCharts: true,
+    enableRangeSelection: true,
+    suppressContextMenu: false,
+    defaultColDef: {
+      suppressMenu: false,
+      filter: 'agTextColumnFilter'
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+        <AgGridReact
+          gridOptions={gridOptions}
+          rowData={badChartData.data}>
+            {columns.map(col => <AgGridColumn field={col.name} key={col.name}></AgGridColumn>)}
+        </AgGridReact>
+      </div>
+
+      <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+        <AgGridReact
+          gridOptions={gridOptions}
+          rowData={goodChartData.data}>
+            {columns.map(col => <AgGridColumn field={col.name} key={col.name}></AgGridColumn>)}
+        </AgGridReact>
+      </div>
     </div>
   );
 }
 
+LicenseManager.setLicenseKey("For_Trialing_ag-Grid_Only-Not_For_Real_Development_Or_Production_Projects-Valid_Until-27_November_2021_[v2]_MTYzNzk3MTIwMDAwMA==bcaae24d146da8e49838739128d51330");
 export default App;
